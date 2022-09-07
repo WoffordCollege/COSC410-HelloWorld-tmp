475c8051087f0ba2e28f314e272a169b12beddb8
Aaron Garrett <aaron.lee.garrett@gmail.com>
2022-09-07 13:50:13 -0400

    Add Juan to the output

--- src/main/java/edu/wofford/Main.java
+++ src/main/java/edu/wofford/Main.java
@@ -4,6 +4,7 @@ package edu.wofford;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Juan is Juan");
 	    if (args.length == 0) {
 	        GuiMain.main(null);
 	    } else {
